c681534b5377f843f7e7daceb7f3bb35a996da98
Ádám Tóth <thevipvr3@gmail.com>
2023-12-30 19:36:58 +0100

    Added margin-bottom to references

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const hamburger = document.querySelector(".hamburger")
-const navbar = document.querySelector("nav")
 const menu = document.querySelector(".menu")
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("active")
