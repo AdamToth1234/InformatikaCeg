f2ea5f78d486e9aa082ac49b8e37ac2062d3428e
Tóth Ádám <thevipvr3@gmail.com>
2024-01-10 20:07:30 +0100

    Created webshop basics

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,22 @@ hamburger.addEventListener("click", ()=>{
 })
 
 const main = [...document.querySelector("main").children]
-console.log(main);
+const products = [...document.querySelectorAll(".product")]
+console.log(products);
+
+
+let loading = 0
 
 main.forEach((item, i) => {
+    loading += 1
     setTimeout(() => {
         item.style.opacity = 1
-    }, i * 100)
+    }, i * 250 * loading)
+})
+
+products.forEach((item, i) => {
+    loading *= 0.7
+    setTimeout(() => {
+        item.style.opacity = 1
+    }, i * 250 * loading)
 })
