6d1a5aa684b49ec0dbcf266120707953b12508c3
Adam Toth <tothadam1010@gmail.com>
2024-01-10 16:49:40 +0100

    Little changes

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,12 @@ const menu = document.querySelector(".menu")
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("active")
 })
+
+const main = [...document.querySelector("main").children]
+console.log(main);
+
+main.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1
+    }, i * 100)
+})
